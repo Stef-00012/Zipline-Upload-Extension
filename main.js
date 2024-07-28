b0e3be661c7b3ae5dc73446408b9e1e4bc657d75
Stef-00012 <stefano.delprete.08@gmail.com>
2024-07-28 09:38:55 +0200

    support vanity url on shorten

--- main.js
+++ main.js
@@ -159,7 +159,15 @@ chrome.contextMenus.onClicked.addListener(async (info) => {
 		}
 
 		case "Zipline_Shorten_URL": {
-			await shortenWithZipline(info.linkUrl);
+			await chrome.storage.local.set({
+				shortenUrl: info.linkUrl
+			})
+
+			await chrome.action.setPopup({
+				popup: 'popups/shorten/shorten.html'
+			})
+
+			await chrome.action.openPopup()
 
 			break;
 		}
@@ -287,89 +295,4 @@ async function uploadToZipline(formData) {
 function convertLink(url) {
 	const urlObj = new URL(url);
 	return `${urlObj.protocol}//${urlObj.host}/*`;
-}
-
-async function shortenWithZipline(url) {
-	const { ziplineUrl, ziplineToken } = await chrome.storage.local.get([
-		"ziplineUrl",
-		"ziplineToken",
-	]);
-
-	if (!ziplineUrl || ziplineUrl === "UNSET")
-		return chrome.notifications.create({
-			title: "Error",
-			message: "Please set your Zipline URL first.",
-			type: "basic",
-			iconUrl: chrome.runtime.getURL("icons/512.png"),
-		});
-
-	if (!urlRegex.test(ziplineUrl))
-		return chrome.notifications.create({
-			title: "Error",
-			message: "Your Zipline URL is not a valid URL.",
-			type: "basic",
-			iconUrl: chrome.runtime.getURL("icons/512.png"),
-		});
-
-	if (!ziplineToken || ziplineToken === "UNSET")
-		return chrome.notifications.create({
-			title: "Error",
-			message: "Please set your Zipline token first.",
-			type: "basic",
-			iconUrl: chrome.runtime.getURL("icons/512.png"),
-		});
-
-	try {
-		const res = await fetch(`${ziplineUrl}/api/shorten`, {
-			body: JSON.stringify({
-				url: url,
-				vanity: null,
-			}),
-			method: "POST",
-			headers: {
-				Authorization: ziplineToken,
-				"Content-Type": "application/json",
-			},
-		});
-
-		if (!res.ok) {
-			const error = await res.json();
-			
-			return chrome.notifications.create({
-				title: "Error",
-				message: `Something went wrong...\nError ${error.code}: ${error.error}.`,
-				type: "basic",
-				iconUrl: chrome.runtime.getURL("icons/512.png"),
-			});
-		}
-
-		const data = await res.json();
-
-		if (data) {
-			return chrome.notifications.create({
-				title: "Success",
-				message: `The link has been shortened as ${data.url}.`,
-				type: "basic",
-				iconUrl: chrome.runtime.getURL("icons/512.png"),
-			});
-		}
-
-		return chrome.notifications.create({
-			title: "Error",
-			message:
-				"Something went wrong...\nPlease report this issue at https://github.com/Stef-00012/Zipline-Upload-Extension/issues.",
-			type: "basic",
-			iconUrl: chrome.runtime.getURL("icons/512.png"),
-		});
-	} catch (e) {
-		console.log(e);
-
-		return chrome.notifications.create({
-			title: "Error",
-			message:
-				"Something went wrong...\nPlease report this issue at https://github.com/Stef-00012/Zipline-Upload-Extension/issues.",
-			type: "basic",
-			iconUrl: chrome.runtime.getURL("icons/512.png"),
-		});
-	}
 }
---
+++ popups/shorten/shorten.js
@@ -0,0 +1,110 @@
+await chrome.action.setPopup({
+	popup: 'popups/settings/settings.html'
+})
+
+const { shortenUrl } = await chrome.storage.local.get(['shortenUrl'])
+
+const settings = [
+    'url',
+    'vanity'
+]
+
+const urlElement = document.getElementById('url')
+const vanityElement = document.getElementById('vanity')
+
+urlElement.value = shortenUrl
+
+const urlRegex = /^http:\/\/(.*)?|https:\/\/(.*)?$/;
+
+document.getElementById('shorten').onclick = async () => {
+    await shortenWithZipline(urlElement.value, vanityElement.value || null)
+
+	await chrome.storage.local.remove(['shortenUrl'])
+
+	await window.close()
+}
+
+async function shortenWithZipline(url, vanity) {
+	const { ziplineUrl, ziplineToken } = await chrome.storage.local.get([
+		"ziplineUrl",
+		"ziplineToken",
+	]);
+
+	if (!ziplineUrl || ziplineUrl === "UNSET")
+		return chrome.notifications.create({
+			title: "Error",
+			message: "Please set your Zipline URL first.",
+			type: "basic",
+			iconUrl: chrome.runtime.getURL("icons/512.png"),
+		});
+
+	if (!urlRegex.test(ziplineUrl))
+		return chrome.notifications.create({
+			title: "Error",
+			message: "Your Zipline URL is not a valid URL.",
+			type: "basic",
+			iconUrl: chrome.runtime.getURL("icons/512.png"),
+		});
+
+	if (!ziplineToken || ziplineToken === "UNSET")
+		return chrome.notifications.create({
+			title: "Error",
+			message: "Please set your Zipline token first.",
+			type: "basic",
+			iconUrl: chrome.runtime.getURL("icons/512.png"),
+		});
+
+	try {
+		const res = await fetch(`${ziplineUrl}/api/shorten`, {
+			body: JSON.stringify({
+				url: url,
+				vanity: vanity || null,
+			}),
+			method: "POST",
+			headers: {
+				Authorization: ziplineToken,
+				"Content-Type": "application/json",
+			},
+		});
+
+		if (!res.ok) {
+			const error = await res.json();
+			
+			return chrome.notifications.create({
+				title: "Error",
+				message: `Something went wrong...\nError ${error.code}: ${error.error}.`,
+				type: "basic",
+				iconUrl: chrome.runtime.getURL("icons/512.png"),
+			});
+		}
+
+		const data = await res.json();
+
+		if (data) {
+			return chrome.notifications.create({
+				title: "Success",
+				message: `The link has been shortened as ${data.url}.`,
+				type: "basic",
+				iconUrl: chrome.runtime.getURL("icons/512.png"),
+			});
+		}
+
+		return chrome.notifications.create({
+			title: "Error",
+			message:
+				"Something went wrong...\nPlease report this issue at https://github.com/Stef-00012/Zipline-Upload-Extension/issues.",
+			type: "basic",
+			iconUrl: chrome.runtime.getURL("icons/512.png"),
+		});
+	} catch (e) {
+		console.log(e);
+
+		return chrome.notifications.create({
+			title: "Error",
+			message:
+				"Something went wrong...\nPlease report this issue at https://github.com/Stef-00012/Zipline-Upload-Extension/issues.",
+			type: "basic",
+			iconUrl: chrome.runtime.getURL("icons/512.png"),
+		});
+	}
+}
