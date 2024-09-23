a1dbf40da9af7da369b64ae64639b217fe256e5a
Stef-00012 <stefano.delprete.08@gmail.com>
2024-09-24 00:15:33 +0200

    add import/export settings

--- src/popups/settings/settings.js
+++ src/popups/settings/settings.js
@@ -1,4 +1,7 @@
 const translationElements = document.querySelectorAll('[data-translation]')
+const importButton = document.getElementById('importSettings')
+const exportButton = document.getElementById('exportSettings')
+const filePicker = document.getElementById('filePicker')
 
 for (const translationElement of translationElements) {
 	const translationId = translationElement.dataset.translation
@@ -81,4 +84,61 @@ for (const setting of settings) {
 			}
 		}
 	};
+}
+
+importButton.onclick = async () => {
+	filePicker.click()
+}
+
+exportButton.onclick = async () => {
+	const exportData = {}
+
+	for (const setting of settings) {
+		const settingContent = await chrome.storage.local.get(setting)
+		console.log(settingContent[setting])
+		exportData[setting] = settingContent[setting]
+	}
+
+	const stringifiedSettings = JSON.stringify(exportData, null, 4)
+
+	const blob = new Blob([stringifiedSettings], {
+		type: "application/json"
+	})
+
+	const link = document.createElement('a')
+	link.href = URL.createObjectURL(blob)
+	link.download = "ziplineSettings.json"
+
+	document.body.appendChild(link)
+	link.click()
+	document.body.removeChild(link)
+}
+
+filePicker.onchange = async (event) => {
+	const file = event.target.files[0]
+
+	if (!file) return;
+
+	const reader = new FileReader()
+
+	reader.onload = async (fileData) => {
+		const jsonContent = fileData.target.result
+		let jsonSettings = {};
+
+		try {
+			jsonSettings = JSON.parse(jsonContent)
+		} catch(e) {
+			console.log(e)
+		}
+
+		for (const setting in jsonSettings) {
+			if (!settings.includes(setting)) continue;
+
+			await chrome.storage.local.set({
+				[setting]: jsonSettings[setting]
+			})
+		}
+	}
+
+	reader.readAsText(file)
 }
